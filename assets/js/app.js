ad24a4b0654d0835d1f3edfa62f557c972c17e16
Anton Nebera <neberaanton@gmail.com>
2017-08-07 00:44:40 +0300

    #1 upload online-shopping project

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+  $('.bxslider').bxSlider({
+  	adaptiveHeight: true,
+  	mode: 'fade',
+  	controls: true
+  });
+  
+})();
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiYXBwLmpzIiwic291cmNlc0NvbnRlbnQiOlsiKGZ1bmN0aW9uKCkge1xyXG4gICd1c2Ugc3RyaWN0JztcclxuICAkKCcuYnhzbGlkZXInKS5ieFNsaWRlcih7XHJcbiAgXHRhZGFwdGl2ZUhlaWdodDogdHJ1ZSxcclxuICBcdG1vZGU6ICdmYWRlJyxcclxuICBcdGNvbnRyb2xzOiB0cnVlXHJcbiAgfSk7XHJcbiAgXHJcbn0pKCk7Il19
--- js/navbar-phone.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-    $('.sidebar-link').on('click', function(e){
-    	e.preventDefault();
-    	$(this).toggleClass('pushed');
-    	$('.blog-nav').toggleClass('blog-nav_mobile');
-    });
-});
