1eff8c64200051ca8f23c689fd50e02f167a972f
Nebera Anton <anebera@apextech.com.ua>
2020-11-05 19:39:31 +0200

    - update the project structure - add gif-test folder

--- portfolio/js/fullscreen-menu.js
+++
@@ -1,9 +0,0 @@
-$('.hamburger-icon').on('click', function(e) {
-	e.preventDefault();
-	$('.menu-container').show();
-});
-
-$('.x-icon__container').on('click', function(e) {
-	e.preventDefault();
-	$('.menu-container').hide();
-});
